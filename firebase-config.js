2f07b80800be3f7c6bfe5402887e26de2656163d
Abdullah-al-Mustafa <letsconnectcpb@gmail.com>
2026-07-13 17:29:02 +0600

    Add Firebase configuration and initialization

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCdBqmMMT_nm6WqonDZ5naHi2-Z1-J4K6g",
+  authDomain: "cholo-porte-boshi-61066.firebaseapp.com",
+  projectId: "cholo-porte-boshi-61066",
+  storageBucket: "cholo-porte-boshi-61066.firebasestorage.app",
+  messagingSenderId: "763591395469",
+  appId: "1:763591395469:web:03b97720f73a3d7a6c3ac4"
+};
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
