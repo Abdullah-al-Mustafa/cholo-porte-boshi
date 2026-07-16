1c4dfdaa63f1b346f0f42c7c4080badf1fcc4f0a
Abdullah-al-Mustafa <letsconnectcpb@gmail.com>
2026-07-16 16:15:08 +0600

    Added class-wise subjects (NCTB) to MCQ/CQ/SQ test pages, static + Firestore mix

---
+++ js/subjects.js
@@ -0,0 +1,7 @@
+// Subjects per class (Bangladesh NCTB Curriculum)
+const classSubjects = {
+  '6': ['Bangla', 'English', 'Mathematics', 'Science', 'Social Science', 'Islamic Studies', 'ICT'],
+  '7': ['Bangla', 'English', 'Mathematics', 'Science', 'Social Science', 'Islamic Studies', 'ICT'],
+  '8': ['Bangla', 'English', 'Mathematics', 'Science', 'Social Science', 'Islamic Studies', 'ICT'],
+  '9-10': ['Bangla', 'English', 'Mathematics', 'Physics', 'Chemistry', 'Biology', 'Social Science', 'Islamic Studies', 'ICT', 'Business Studies', 'Economics', 'Geography', 'History', 'Civics']
+};
