649540aa58f71f36021f97807c63a04d7277ccba
Abdullah-al-Mustafa <letsconnectcpb@gmail.com>
2026-07-16 16:16:45 +0600

    Added group selector (Science/Commerce/Arts) for class 9-10 in MCQ/CQ/SQ tests

--- js/subjects.js
+++ js/subjects.js
@@ -3,5 +3,9 @@ const classSubjects = {
   '6': ['Bangla', 'English', 'Mathematics', 'Science', 'Social Science', 'Islamic Studies', 'ICT'],
   '7': ['Bangla', 'English', 'Mathematics', 'Science', 'Social Science', 'Islamic Studies', 'ICT'],
   '8': ['Bangla', 'English', 'Mathematics', 'Science', 'Social Science', 'Islamic Studies', 'ICT'],
-  '9-10': ['Bangla', 'English', 'Mathematics', 'Physics', 'Chemistry', 'Biology', 'Social Science', 'Islamic Studies', 'ICT', 'Business Studies', 'Economics', 'Geography', 'History', 'Civics']
+  '9-10': {
+    'Science': ['Bangla', 'English', 'Mathematics', 'Physics', 'Chemistry', 'Biology', 'ICT'],
+    'Commerce': ['Bangla', 'English', 'Mathematics', 'Accounting', 'Business Studies', 'Economics', 'ICT'],
+    'Arts': ['Bangla', 'English', 'Mathematics', 'History', 'Geography', 'Civics', 'ICT']
+  }
 };
